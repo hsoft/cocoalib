6c898814ad7c01f1e1e0a1f13e9f9c7867e7d032
Virgil Dupras <hsoft@hardcoded.net>
2010-02-11 17:50:47 +0100

    Removed the old Outline.

--- PyApp.h
+++ PyApp.h
@@ -11,23 +11,9 @@ http://www.hardcoded.net/licenses/bsd_license
 #import "PyRegistrable.h"
 
 @interface PyApp: PyRegistrable <Worker>
-//Send NSArray instead of NSIndexPath. Working with NSIndexPath sucks in Python.
-//Use [Utils indexPath2Array:]
-//Data
-// 0 if none. Returning a max level saves a lot of childCountForIndexes: calls
-- (NSInteger)getOutlineViewMaxLevel:(NSInteger)tag;
-// returns an array of the counts of the subitems
-- (NSArray *)getOutlineView:(NSInteger)tag childCountsForPath:(NSArray *)indexPath;
-- (NSArray *)getOutlineView:(NSNumber *)tag valuesForIndexes:(NSArray *)indexPath;
-// 0 = unmarked, 1 = marked, 2 = unmarkable
-- (NSNumber *)getOutlineView:(NSNumber *)tag markedAtIndexes:(NSArray *)indexPath;
-
-- (NSNumber *)getTableViewCount:(NSNumber *)tag;
-- (NSArray *)getTableViewMarkedIndexes:(NSNumber *)tag;
-- (NSArray *)getTableView:(NSNumber *)tag valuesForRow:(NSNumber *)row;
-
 //Worker
 - (NSNumber *)getJobProgress;
 - (NSString *)getJobDesc;
 - (void)cancelJob;
+- (void)jobCompleted:(id)jobid;
 @end
