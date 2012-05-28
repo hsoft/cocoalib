d4244179e3ac82ae12d1d0b486a56fa9f25f8763
Virgil Dupras <hsoft@hardcoded.net>
2012-05-28 14:42:20 -0400

    Forgot to add HSFairwareProtocol in last commit.

---
+++ HSFairwareProtocol.h
@@ -0,0 +1,21 @@
+/* 
+Copyright 2012 Hardcoded Software (http://www.hardcoded.net)
+
+This software is licensed under the "BSD" License as described in the "LICENSE" file, 
+which should be included with this package. The terms are also available at 
+http://www.hardcoded.net/licenses/bsd_license
+*/
+
+#import <Cocoa/Cocoa.h>
+
+@protocol HSFairwareProtocol
+- (void)initialRegistrationSetup;
+- (NSString *)appName;
+- (NSString *)appLongName;
+- (BOOL)isRegistered;
+- (BOOL)setRegisteredCode:(NSString *)code andEmail:(NSString *)email registerOS:(BOOL)registerOS;
+- (NSNumber *)unpaidHours;
+- (void)contribute;
+- (void)buy;
+- (void)aboutFairware;
+@end
